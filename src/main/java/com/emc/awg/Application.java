dfce0cc5aeca3966697468c8350605b74a767971
Imam Chishty <imam.chishty@emc.com>
2016-04-13 15:29:53 +0400

    Updated the App file in prep of the filters/postgres.

--- src/main/java/com/emc/awg/Application.java
+++ src/main/java/com/emc/awg/Application.java
@@ -1,7 +1,11 @@
 package com.emc.awg;
 
+import com.shedhack.trace.request.filter.RequestTraceFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,5 +14,15 @@ public class Application {
 
         SpringApplication.run(Application.class, args);
     }
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+/*    @Bean
+    public FilterRegistrationBean requestIdFilterRegistrationBean() {
+        FilterRegistrationBean filter = new FilterRegistrationBean();
+        filter.setFilter(new RequestTraceFilter(appName, null));
+        return filter;
+    }*/
 
 }
