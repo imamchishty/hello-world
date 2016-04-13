e69b0c8db3b774f7eadac4ba0f32ef29ad4629f7
Imam Chishty <imam.chishty@emc.com>
2016-04-13 15:45:14 +0400

    Failed

--- src/main/java/com/emc/awg/Application.java
+++ src/main/java/com/emc/awg/Application.java
@@ -1,11 +1,8 @@
 package com.emc.awg;
 
-import com.shedhack.trace.request.filter.RequestTraceFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
